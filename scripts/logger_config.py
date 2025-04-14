58fb9bed8ad69ccb0a2b4d3ad992273112e443d5
Weronika <weronikapodyma97@gmail.com>
2025-04-14 12:08:53 +0200

    add path for logs

--- scripts/logger_config.py
+++ scripts/logger_config.py
@@ -1,10 +1,11 @@
 import logging
+import os 
 
 def setup_logger(name):
     logger = logging.getLogger(name)
     logging.basicConfig(
         level=logging.INFO,
-        filename='app.log',
+        filename=os.path.join('logs','app.log'),
         encoding='utf-8',
         filemode='a',
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
