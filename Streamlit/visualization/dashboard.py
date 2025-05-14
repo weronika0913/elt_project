75b729f939bec81205bf699fffc3dd26051b9604
Weronika <weronikapodyma97@gmail.com>
2025-05-14 23:04:23 +0200

    add streamlit service and web app

--- Streamlit/visualization/dashboard.py
+++ Streamlit/visualization/dashboard.py
@@ -1,14 +1,14 @@
 import streamlit as st
 import duckdb
 import pandas as pd
-import plotly.express as px
 import os
+import altair as alt 
 
 # ---- UI DASHBOARDU ----
-st.set_page_config(page_title="CoinCap Analysis", layout="wide")
-
-#Connection to db
-duckdb_path = "/opt/airflow/data/CoinCap.db"
+st.set_page_config(page_title="CoinCap Analysis", 
+                   layout="centered")
+st.title("CoinCap Analysis")
+duckdb_path = "/app/data/CoinCap.db"
 
 if not os.path.exists(duckdb_path):
     st.info("Waiting for input data")
@@ -16,60 +16,69 @@ else:
    conn = duckdb.connect(duckdb_path)
 
 
-# #fetch data
-# @st.cache_data # decorator for caching data
-# def load_data_from_db():
-#     return conn.execute("SELECT priceUSD, circulatingSupply, date FROM bitcoin").fetchdf()
+if st.button("🔄 Refresh data"):
+    st.cache_data.clear()
+    st.rerun()
 
-# if st.button("🔄 Refresh data"):
-#     st.cache_data.clear()
-#     st.rerun()
+@st.cache_data
+def load_history_price():
+    df = conn.execute("""
+    SELECT *
+    FROM history_price
+    """).fetchdf()
+    conn.close()
+    return df
 
-# df = load_data_from_db()
-# df_transformed = df.rename(columns={"priceUsd": "Price (USD)", "date": "Date", "circulatingSupply": "Circulating Supply (BTC)"})
+df = load_history_price()
 
-# # -- HEADER --
-# st.title("📈 Bitcoin Price Dashboard")
-# st.write("10 most recent historical data of Bitcoin prices")
+unique_asset = df["asset_id"].drop_duplicates()
 
-# # ---- TABLE ----
-# st.subheader("🔍 Data Preview")
-# first_10_rows = df_transformed.sort_values(by="Date", ascending=False).head(10)
-# st.dataframe(first_10_rows)
-
-# # ---- SIDE BAR ----
-# st.sidebar.header("📌 Please Filter Here:")
-# start_date = st.sidebar.date_input("Select Start Date", df["date"].min())
-# end_date = st.sidebar.date_input("Select End Date", df["date"].max())
-
-# df_filtered = df_transformed[(df_transformed["Date"] >= pd.Timestamp(start_date)) & (df_transformed["Date"] <= pd.Timestamp(end_date))]
-
-# # ---- LINE CHART PRICE ----
-# st.subheader("📊 Bitcoin price chart")
-# fig = px.line(df_filtered,
-#                 x="Date",
-#                 y="Price (USD)",
-#                 title="Bitcoin price over time",
-#                 )
-# st.plotly_chart(fig)
-
-# # ---- LINE CHART CIRCULATING ----
-# st.subheader("📊 Circulating Supply of Bitcoin")
-# fig = px.line(df_filtered,
-#                 x="Date",
-#                 y="Circulating Supply (BTC)",
-#                 title="Bitcoin Circulating Supply Over Time")
-# st.plotly_chart(fig)
+# ---- SIDE BAR ---
+st.sidebar.header("📌 Please Filter Here:")
+start_date = st.sidebar.date_input("Select Start Date", df["date_id"].min())
+end_date = st.sidebar.date_input("Select End Date", df["date_id"].max())
+asset = st.sidebar.selectbox("Select asset", unique_asset)
 
 
-# # ---- SCATTER PLOT OF PRICE VS VOLUME ----
-# st.subheader("📊 Bitcoin Price vs Circulating Supply")
+# ----- LINE CHART for price -----
+st.write("Price Change of Selected Asset Over Time")
+df_filtered = df[(df["date_id"].dt.date >= start_date) & (df["date_id"].dt.date <= end_date) & (df["asset_id"] == asset)]
 
-# fig = px.scatter(df_filtered, 
-#                  x="Circulating Supply (BTC)", 
-#                  y="Price (USD)",
-#                  title="Bitcoin Price vs Circulating Supply"
-#                  )
-# st.plotly_chart(fig)
 
-# conn.close()
+
+# Calculate Max and Min prices
+max_price = df_filtered["price_usd"].max()
+min_price = df_filtered["price_usd"].min()
+
+# Use columns to display the cards side by side
+col1, col2 = st.columns(2)
+
+# Max Price - wheel card
+col1.markdown(
+    f"""
+    <div style="background-color: #81C784; color: white; width: 160px; height: 160px; 
+                border-radius: 50%; display: flex; flex-direction: column; 
+                align-items: center; justify-content: center; 
+                box-shadow: 0 6px 12px rgba(0, 0, 0, 0.25); 
+                font-size: 16px; text-align: center; margin: auto;">
+        <b>Max</b>
+        <div style="font-size: 22px; font-weight: bold;">${max_price:,.2f}</div>
+    </div>
+    """, unsafe_allow_html=True)
+
+# Min Price - wheel card
+col2.markdown(
+    f"""
+    <div style="background-color: #FF8A65; color: white; width: 160px; height: 160px; 
+                border-radius: 50%; display: flex; flex-direction: column; 
+                align-items: center; justify-content: center; 
+                box-shadow: 0 6px 12px rgba(0, 0, 0, 0.25); 
+                font-size: 16px; text-align: center; margin: auto;">
+        <b>Min</b>
+        <div style="font-size: 22px; font-weight: bold;">${min_price:,.2f}</div>
+    </div>
+    """, unsafe_allow_html=True)
+
+
+st.write("")
+st.line_chart(df_filtered.set_index("date_id")["price_usd"])
