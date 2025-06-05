4d5cc76dafb5182f6a9b64d4ad6d2f6bceda14ab
Weronika <weronikapodyma97@gmail.com>
2025-06-05 16:21:02 +0200

    refactor extractor

--- src/extractor.py
+++ src/extractor.py
@@ -6,193 +6,118 @@ import os
 import pytz
 import duckdb
 from logger_config import setup_logger
-from dotenv import load_dotenv
+from auth import BaseApiAuth
 
-#logger configuration
-# This logger will be used to log messages in the script
-logger = setup_logger(__name__)
+class CoinCapExtractor:
+    """
+    The CoinCapExtractor class is designed to dynamically construct API endpoint URLs 
+    for accessing cryptocurrency data from the CoinCap API. It allows users to specify 
+    an endpoint template containing path parameters and optional query parameters.
 
+    Attributes:
+        endpoint (str):
+          The specific API endpoint to call (e.g., 'history', 'rates').
 
-
-class CoinDataExtractor:
-    def __init__(self, asset_id, start_year=2024,interval="d1"):
-        self.asset_id = asset_id
-        self.start_year = start_year
-        self.end_date = int(datetime.now().year)
-        self.interval = interval
-        self.url = f"https://rest.coincap.io/v3/assets/{asset_id}/history"        
-
-    def _get_token(self):
-
-        load_dotenv()
-        api_key = os.getenv("API_KEY")
-        return {
-            "Authorization": f"Bearer {api_key}"
-            }
-
-    # This method fetches historical coin data from the CoinCap API for the specified years and saves it as CSV files.
-    def get_coin_data(self):
-        utc_zone = pytz.UTC
-
-        self.file_names = []
-        for year in range(self.start_year, self.end_date + 1):
-            file_name = f"{self.asset_id}-{year}"
-            self.file_names.append(file_name)
-            start_date = datetime(year,1,1)
-            
-            # Convert start_date to UTC timezone as API expects UTC timestamps
-            start_of_year = utc_zone.localize(start_date.replace(hour=00, minute=00,second=00, microsecond=000000))
-            end_of_year = utc_zone.localize(start_date.replace(month=12, day=31, hour=23, minute=59,second=59, microsecond=999999))
-            
-            start_timestamp = int(start_of_year.timestamp() * 1000)
-            end_timestamp = int(end_of_year.timestamp() * 1000)
-
+        path_params (list): 
+            A list of strings representing path values (e.g., asset IDs such as 'bitcoin', 'ethereum') 
+            that will replace the `{path}` placeholder in the endpoint template.
         
-            #parameters for the API request
-            params = {
-                "interval": self.interval,
-                "start": start_timestamp,
-                "end": end_timestamp
-            }
-            try:
-                response = requests.get(self.url, params=params, headers=self._get_token())
-                response.raise_for_status()
-            
-            except requests.exceptions.RequestException as e:
-                logger.error(f"Error fetching data: {e}")
-                return
-            try:
-                data = response.json()
-                assets = data["data"]
-                keys = assets[0].keys()
-                #Save the data to a CSV file
-                with open(os.path.join("temp",file_name), "w") as file:
-                    dict_writer = csv.DictWriter(file,fieldnames=keys)
-                    dict_writer.writeheader()
-                    dict_writer.writerows(assets)
-                logger.info(f"Data successfully fetched for {self.asset_id}, year: {year}")
-            except Exception as e:
-                logger.error(f"Error fetching for {self.asset_id}, year: {year}") 
-                raise
+        params (dict): 
+            A dictionary of optional query parameters (e.g., {'interval': 'd1', 'start': ..., 'end': ...}).
+    """
+    def __init__(self, endpoint: str, path_params: list = None, params: dict = None):
+        self.endpoint = endpoint
+        self.path_params = path_params or []
+        self.params = params or {}
 
 
+    def _build_full_endpoint(self):
+        """
+    Iterates over the `path_params` list and formats the `endpoint` string by substituting 
+        the `{path}` placeholder with each individual path value. 
+        Returns a list of fully formatted endpoint URLs.
 
-    # This method loads the data from the CSV file into Minio.
-    def upload_file_to_minio(self):
-        minio_client = get_minio_client()
-        logger.info(f"file_names = {self.file_names}")
-        for file_list in self.file_names:
-            for file_name in file_list:
-                local_path = os.path.join("temp",file_name)
-                try:
-                    minio_client.fput_object(self.asset_id, file_name, local_path)
-                    os.remove(local_path)
-                    logger.info(f"File uploaded to Minio successfully")
-                except Exception as e:
-                    logger.error(f"Error uploading file to Minio: {e}")
-                    raise
+    Example:
+            extractor = CoinCapExtractor(
+                endpoint="assets/{path}/history",
+                path_params=["bitcoin", "ethereum"]
+            )
+            urls = extractor._build_full_endpoint()
+            # Output: ['assets/bitcoin/history', 'assets/ethereum/history']           
+                """
+        urls = []
+        for path in self.path_params:
+            url = self.endpoint.format(path=path)
+            urls.append(url)
+        return urls
+    
+    def _full_url(self):
+        """
+        Combines base URL from BaseApiAuth with formatted endpoints
+
+        Returns:
+            list[str]: A list of full URLs 
+        """
+        base = BaseApiAuth().full_url
+        full_urls = []
+
+        for endpoint in self._build_full_endpoint():
+            url = f"{base}{endpoint}"
+            full_urls.append(url)
+        return full_urls
+    
+    def date_conversion(self):
+        """
+        Converts 'start' and 'end' date strings from the parameters dictionary to UNIX timestamps in milliseconds.
+
+        This method expects the 'params' attribute to contain both 'start' and 'end' keys with date values 
+        formatted as 'YYYY-MM-DD'. It validates the presence of these keys and parses the string dates into 
+        timezone-aware UTC datetime objects. The resulting datetime objects are then converted to UNIX timestamps 
+        in milliseconds (as required by the CoinCap API) and the original 'params' dictionary is updated accordingly.
+
+        Raises:
+            ValueError: If either 'start' or 'end' date is missing in the 'params' dictionary.
+
+        Returns:
+            dict: The updated 'params' dictionary with 'start' and 'end' values as UNIX time in milliseconds.
+            """
         
-    # This method creates a bucket in MinIO if it does not already exist.
-    def create_bucket(self): 
-        minio_client = get_minio_client()
-        try:
-            if not minio_client.bucket_exists(self.asset_id):
-                minio_client.make_bucket(self.asset_id)
-            logger.info(f"Succesfully bucket {self.asset_id} was created")
-        except Exception as e:
-            logger.error(f"Error creating bucket {self.asset_id}")
-            raise
-    
-    
-
+        start_str = self.params.get("start")
+        end_str = self.params.get("end")
+        if not start_str or not end_str:
+            raise ValueError("Missing 'start' or 'end' date in params attribute")
             
-    # #This method returns the last load date from DuckDB if it exists
-    # # If no data exists, it provides information to perform the initial load.
-    # def get_last_load(self):
-    #     db_path = os.path.join("data","CoinCap.db")
-    #     conn = duckdb.connect(db_path)
-    #     date_df = conn.execute("SELECT MAX(date_id) FROM history_price").fetchdf()
-    #     try:
-    #         if not date_df.empty:
-    #             date = date_df.iloc[0,0]
-    #             next_day = date + timedelta(days=1)
-    #             next_day_str = next_day.strftime("%Y-%m-%d")
-    #             logger.info(f"Last load date: {date.strftime('%Y-%m-%d')}")
-    #             logger.info(f"Next load date: {next_day_str}")
-    #             return next_day_str
-    #         else:
-    #             logger.info("No data found in DuckDB. Please first run the initial load.")
-    #     finally:
-    #         conn.close()
- 
+        else:
+            start_date_obj = pytz.UTC.localize(datetime.strptime(start_str,"%Y-%m-%d"))
+            end_date_obj = pytz.UTC.localize(datetime.strptime(end_str,"%Y-%m-%d"))
+
+            self.params["start"] = int(start_date_obj.timestamp() * 1000)
+            self.params["end"] = int(end_date_obj.timestamp() * 1000)
+        return self.params
     
-    # This method downloads CSV files from MinIO and loads the data into DuckDB's staging and history tables.
-    def load_data_from_minio_to_duckdb(self, db_name: str = "CoinCap.db"):
-        minio_client = get_minio_client()
-        for file_list in self.file_names:
-            logger.info(file_list)
-            for file_name in file_list:
-                try:
-                    
-                    local_file_path = os.path.join("temp",file_name)
-                    
-                    #init duckdb connection
-                    db_path = os.path.join("data",db_name)
-                    conn = duckdb.connect(db_path)
+    def get_data(self):
+        base = BaseApiAuth()
+        token = base.get_token()
+        if self.params.get("start") and self.params.get("end"):
+                self.date_conversion()
 
-                    # #load file from minio
-                    minio_client.fget_object(self.asset_id,file_name,local_file_path)
+        responses = []
+        urls = self._full_url()
 
-                    
-                    list_of_variables =[self.asset_id,local_file_path]
-                    print(list_of_variables)
+        for url in urls:
+            response = requests.get(url,params=self.params,headers=token)
+            data = response.json()
+            responses.append(data)
+        return responses
 
-                    #load into staging history_price
-                    conn.execute("""
-                        WITH temp_csv AS (
-                            SELECT
-                                $1 as asset_id,
-                                priceUsd,
-                                time,
-                                date,
-                                current_timestamp as load_timestamp
-                            FROM read_csv($2)
-                        )
-                        INSERT INTO staging_history_price
-                        SELECT *
-                        FROM temp_csv
-                        WHERE NOT EXISTS (
-                            SELECT 1
-                            FROM staging_history_price as s
-                            WHERE s.asset_id = temp_csv.asset_id AND s.date = temp_csv.date
-                        );
-                    """, list_of_variables)
 
-                    # remove records that have the same date as the currently loaded data
-                    conn.execute("""
-                        DELETE FROM history_price WHERE asset_id = $1 AND date_id IN (SELECT date FROM staging_history_price WHERE asset_Id = $1)""",[self.asset_id])
 
-                    # inserting into history_price
-                    conn.execute("""
-                    INSERT INTO history_price
-                    SELECT 
-                        stag.asset_id,
-                        dd.date_id,
-                        priceUsd,
-                        current_timestamp as load_timestamp
-                    FROM staging_history_price as stag
-                    JOIN dim_date as dd
-                    ON dd.date_id = stag.date
-                    JOIN dim_assets as da
-                    ON da.asset_id = stag.asset_id
-                    WHERE stag.asset_id = $1
-                    """,[self.asset_id])
-                    conn.commit()
-                    conn.close()
+extractor = CoinCapExtractor(
+    endpoint="v3/assets/{path}",
+    path_params=["bitcoin", "ethereum"],
+    params={"start": "2024-01-01", "end": "2024-01-02", "interval": "d1"}
+)
 
-                    #remove temporary file
-                    os.remove(local_file_path)
-                    logger.info(f"Data loaded successfully from {file_name} to history_price table")
-                except Exception as e:
-                    logger.error(f"Error loading data to history_price table: {e} from {file_name}")
-                    raise
+data = extractor.get_data()
+
+print(data)
